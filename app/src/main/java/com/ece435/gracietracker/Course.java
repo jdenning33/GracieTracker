862cec8737fe8061fbb462449783a17d95ece927
Jason Denning <jdenning33@gmail.com>
2017-04-29 08:40:37 -0600

    Major Refactor
    Convert to fragment based rather than activity based.
    Added firebase authentication.
    Refactored classes and skills to be separate.
    Pushed a new Activity Bar rather than the stock AppBar.

--- app/src/main/java/com/ece435/gracietracker/Course.java
+++ app/src/main/java/com/ece435/gracietracker/Course.java
@@ -18,73 +18,77 @@ import java.util.List;
 
 public class Course {
     private int number;             //Course number (1 - 23)
-    private String primarySkill;    //Bolded technique on the course description
-    private String secondarySkill;  //Second technique on the description
-    private int[] skillNumbers;     //The corresponding skills that the course teaches (GU1 - GU36)
+    private int[] primarySkills;    //Bolded technique on the course description
+    private int[] secondarySkills;  //Second technique on the description
 
     //booleans for how many times the course has been completed on this belt
     private boolean[] didComplete = new boolean[] {false, false, false};
 
-
     private Date[] courseOfferings; //array of when the course is planned to be taught
 
 
-    //List of all Gracie Course offerings
-    public static final ArrayList<Course> courseArray = populateCourseArray();
-
-
-    public Course(int number, String primarySkill, String secondarySkill){
+    public Course(int number, int[] primarySkills, int[] secondarySkills){
         this.number = number;
-        this.primarySkill = primarySkill;
-        this.secondarySkill = secondarySkill;
+        this.primarySkills = primarySkills;
+        this.secondarySkills = secondarySkills;
     }
 
     public void toggleComplete(int number){
         didComplete[number] = !didComplete[number];
     }
 
-    public int getNumber(){
-        return number;
-    }
+    public int getNumber(){ return number; }
     public String getPrimaryTechnique(){
-        return primarySkill;
+        return Skill.skillArray.get(primarySkills[0]-1).getName();
     }
     public String getSecondaryTechnique(){
-        return secondarySkill;
+        return Skill.skillArray.get(secondarySkills[0]-1).getName();
+    }
+    public String getPrimaryTechniqueLink(){
+        return Skill.skillArray.get(primarySkills[0]-1).getLink();
+    }
+    public String getSecondaryTechniqueLink(){
+        return Skill.skillArray.get(secondarySkills[0]-1).getLink();
     }
     public boolean getDidComplete(int number){
         return didComplete[number];
     }
 
+
+
+    //List of all Gracie Course offerings
+    public static final ArrayList<Course> courseArray = populateCourseArray();
+
     private static ArrayList<Course> populateCourseArray(){
         String[] columns = new String[] { "_id", "primarySkill", "secondarySkill" };
         ArrayList<Course> courseArray = new ArrayList<Course> (23);
 
-        courseArray.add(new Course( 1 , "Trap and Roll Escape – Mount (GU 1)*"       ,"Leg Hook Takedown (GU 6)"                    ));
-        courseArray.add(new Course( 2 , "Americana Armlock – Mount (GU 2)"           ,"Clinch (Aggressive Opponent) (GU 7)"         ));
-        courseArray.add(new Course( 3 , "Positional Control – Mount (GU 3)"          ,"Body Fold Takedown (GU 14)"                  ));
-        courseArray.add(new Course( 4 , "Take the Back + R.N.C. – Mount (GU 4 + 5)"  ,"Clinch (Conservative Opponent) (GU 15)"      ));
-        courseArray.add(new Course( 5 , "Punch Block Series (1-4) – Guard (GU 8)"    ,"Guillotine Choke (Standing) (GU 23)"         ));
-        courseArray.add(new Course( 6 , "Straight Armlock – Mount (GU 9)"            ,"Guillotine Defense (GU 32)"                  ));
-        courseArray.add(new Course( 7 , "Triangle Choke – Guard (GU 10)"             ,"Haymaker Punch Defense (GU 30)"              ));
-        courseArray.add(new Course( 8 , "Elevator Sweep – Guard (GU 11)"             ,"Rear Takedown (GU 29) "                      ));
-        courseArray.add(new Course( 9 , "Elbow Escape – Mount (GU 12)"               ,"Pull Guard (GU 21)"                          ));
-        courseArray.add(new Course( 10, "Positional Control – Side Mount (GU 13)"    ,"Double Leg Takedown (Aggressive) (GU 17)"    ));
-        courseArray.add(new Course( 11, "Headlock Counters – Mount (GU 16)"          ,"Standing Headlock Defense (GU 26)"           ));
-        courseArray.add(new Course( 12, "Headlock Escape 1 – Side Mount (GU 18)"     ,"Standing Armlock (GU 34)"                    ));
-        courseArray.add(new Course( 13, "Straight Armlock – Guard (GU 19)"           ,"Clinch (Aggressive Opponent) (GU 7)"         ));
-        courseArray.add(new Course( 14, "Double Ankle Sweep – Guard (GU 20)"         ,"Guillotine Choke (Guard Pull) (GU 23)"       ));
-        courseArray.add(new Course( 15, "Headlock Escape 2 – Side Mount (GU 22)"     ,"Clinch (Conservative Opponent) (GU 15)"      ));
-        courseArray.add(new Course( 16, "Shrimp Escape – Side Mount (GU 24)"         ,"Body Fold Takedown (GU 14)"                  ));
-        courseArray.add(new Course( 17, "Kimura Armlock – Guard (GU 25)"             ,"Leg Hook Takedown (GU 6)"                    ));
-        courseArray.add(new Course( 18, "Punch Block Series (5) – Guard (GU 27)"     ,"Haymaker Punch Defense (GU 30)"              ));
-        courseArray.add(new Course( 19, "Hook Sweep – Guard (GU 28)"                 ,"Guillotine Defense (GU 32)"                  ));
-        courseArray.add(new Course( 20, "Take the Back – Guard (GU 31)"              ,"Standing Headlock Defense (GU 26)"           ));
-        courseArray.add(new Course( 21, "Elbow Escape – Side Mount (GU 33)"          ,"Pull Guard (GU 21)"                          ));
-        courseArray.add(new Course( 22, "Twisting Arm Control – Mount (GU 35)"       ,"Rear Takedown (GU 29)"                       ));
-        courseArray.add(new Course( 23, "Double Underhook Pass – Guard (GU 36)"      ,"Double Leg Takedown (Conservative) (GU 17)"  ));
+        courseArray.add(new Course( 1 , new int[] {  1}, new int[] { 6}));
+        courseArray.add(new Course( 2 , new int[] {  2}, new int[] { 7}));
+        courseArray.add(new Course( 3 , new int[] {  3}, new int[] {14}));
+        courseArray.add(new Course( 4 , new int[] {4,5}, new int[] {15}));
+        courseArray.add(new Course( 5 , new int[] {  8}, new int[] {23}));
+        courseArray.add(new Course( 6 , new int[] {  9}, new int[] {32}));
+        courseArray.add(new Course( 7 , new int[] { 10}, new int[] {30}));
+        courseArray.add(new Course( 8 , new int[] { 11}, new int[] {29}));
+        courseArray.add(new Course( 9 , new int[] { 12}, new int[] {21}));
+        courseArray.add(new Course( 10, new int[] { 13}, new int[] {17}));
+        courseArray.add(new Course( 11, new int[] { 16}, new int[] {26}));
+        courseArray.add(new Course( 12, new int[] { 18}, new int[] {34}));
+        courseArray.add(new Course( 13, new int[] { 19}, new int[] { 7}));
+        courseArray.add(new Course( 14, new int[] { 20}, new int[] {23}));
+        courseArray.add(new Course( 15, new int[] { 22}, new int[] {15}));
+        courseArray.add(new Course( 16, new int[] { 24}, new int[] {14}));
+        courseArray.add(new Course( 17, new int[] { 25}, new int[] { 6}));
+        courseArray.add(new Course( 18, new int[] { 27}, new int[] {30}));
+        courseArray.add(new Course( 19, new int[] { 28}, new int[] {32}));
+        courseArray.add(new Course( 20, new int[] { 31}, new int[] {26}));
+        courseArray.add(new Course( 21, new int[] { 33}, new int[] {21}));
+        courseArray.add(new Course( 22, new int[] { 35}, new int[] {29}));
+        courseArray.add(new Course( 23, new int[] { 36}, new int[] {17}));
 
 
         return courseArray;
     }
 }
+
--- app/src/main/java/com/ece435/gracietracker/LoginActivity.java
+++ app/src/main/java/com/ece435/gracietracker/LoginActivity.java
@@ -1,37 +1,306 @@
 package com.ece435.gracietracker;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
     public static final String USER_NAME = "com.ece435.gracietracker.USER_NAME";
+    public static final int RC_SIGN_IN = 111;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Set the toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        ///     Firebase Initialization
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null) {
+                    // User is signed in
+                    Log.d("Login", "onAuthStateChanged:signed:_in" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("Login", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        ///     Google Sign In Initialization
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(LoginActivity.this, "Connection Failed", Toast.LENGTH_SHORT);
+                    }
+                } /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+        SignInButton googleSignInButton = (SignInButton) this.findViewById(R.id.GoogleSignInButton);
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
+
         if( isLoggedIn() ){
-            goToUserHomeView( null );
+            goToMainView( null );
         }
     }
 
-    public String getUserName() {
-        EditText editText = (EditText) findViewById(R.id.editText2);
-        String userName = editText.getText().toString();
-        if(userName == null) return "Guest";
-        return userName;
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    ///     Action Bar Initialization
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.sign_out:
+                FirebaseAuth.getInstance().signOut();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.appbar, menu);
+        return true;
     }
 
-    public void goToUserHomeView(View view) {
-        Intent intent = new Intent(this, UserHomeActivity.class);
-        String userName = getUserName();
-        intent.putExtra(USER_NAME, userName);
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    ///     Control Firebase auth lifecycle
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) goToMainViewHelp();
+
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        if (mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    ///     Firebase Sign In and Sign Up
+    /////////////////////////////////////////////////////////////////////////////////////////////
+
+    // SIGN UP
+    public void createNewUser(View view){
+        EditText emailText = (EditText) findViewById(R.id.EmailText);
+        String email = emailText.getText().toString();
+        EditText passwordText = (EditText) findViewById(R.id.PasswordText);
+        String password = passwordText.getText().toString();
+
+        if(email.length() < 1 || password.length() < 1){
+            Toast.makeText(LoginActivity.this, "Must provide an Email and Password",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        createNewUserHelp(email, password);
+    }
+    public void createNewUserHelp(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Login", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Authentication Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        } else{
+                            Toast.makeText(LoginActivity.this, "You have successfully created an account",
+                                    Toast.LENGTH_SHORT).show();
+                            goToMainViewHelp();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    // SIGN IN
+    public void loginUser(View view){
+        EditText emailText = (EditText) findViewById(R.id.EmailText);
+        String email = emailText.getText().toString();
+        EditText passwordText = (EditText) findViewById(R.id.PasswordText);
+        String password = passwordText.getText().toString();
+
+        if(email.length() < 1 || password.length() < 1){
+            Toast.makeText(LoginActivity.this, "Must provide an Email and Password",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        loginUserHelp(email, password);
+    }
+    public void loginUserHelp(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Login", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w("Login", "signInWithEmail:failed", task.getException());
+                            Toast.makeText(LoginActivity.this, "Could Not Authenticate",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "You Have Successfully Logged In",
+                                    Toast.LENGTH_SHORT).show();
+                            goToMainViewHelp();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    ///     Google Sign In
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    private void googleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+    public void handleSignInResult(GoogleSignInResult result){
+        if (result.isSuccess()) {
+            // Google Sign In was successful, authenticate with Firebase
+            GoogleSignInAccount account = result.getSignInAccount();
+            firebaseAuthWithGoogle(account);
+
+            Toast.makeText(LoginActivity.this, "You Have Successfully Logged In With Google",
+                    Toast.LENGTH_SHORT).show();
+
+            goToMainViewHelp();
+        } else {
+            Toast.makeText(LoginActivity.this, "Google Login Unsuccessful",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("Login", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Login", "signInWithCredential:success");
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goToMainViewHelp();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Login", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Google to Firebase Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    ///     Homebrew Methods
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    public void goToMainView(View view) {
+        goToMainViewHelp();
+    }
+    public void goToMainViewHelp() {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 
     public boolean isLoggedIn() {
